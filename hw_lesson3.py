7d3a8e0683a67a35d8e518701ff3a580b920cff4
swisaswisa11 <adirswisa11@gmail.com>
2022-08-09 18:55:44 +0300

    hw_upload

---
+++ hw_lesson3.py
@@ -0,0 +1,10 @@
+name1 = "raz"
+name2 = "itay"
+name3 = "noam"
+for i in range(1):
+    print("good morning" ,name1)
+    print("good morning" ,name2)
+    print("good morning" ,name3)
+    print("good night" ,name1)
+    print("good night" ,name2)
+    print("good night" ,name3)
---
+++ hw_lesson6.py
@@ -0,0 +1,16 @@
+
+
+
+
+
+
+
+
+num1=20
+while True:
+    num2=int(input("אני רוצה טונה"))
+    if num1>=num2:
+        num1 = num1-num2 
+    else:
+        print("אין לנו")
+        break
